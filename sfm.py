7d05bd3406bf0c46f7ad28f6755245964da89960
Penguin.jpg <sky026912@gmail.com>
2025-01-23 14:46:26 +0800

    use known camera parameters

--- sfm.py
+++ sfm.py
@@ -6,9 +6,13 @@ from hloc import (
     match_dense,
     reconstruction,
     visualization,
-    pairs_from_retrieval,
     triangulation,
+    pairs_from_retrieval,
+    pairs_from_exhaustive,
 )
+from hloc.utils.io import read_image
+from hloc.utils.read_write_model import read_cameras_binary, read_images_binary
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -18,27 +22,72 @@ if __name__ == "__main__":
                         help="Path to the output directory, default: %(default)s")
     args = parser.parse_args()
 
-    images = args.dataset / "images"
-    reference_sfm = args.dataset / "sparse/0"
+    images_path = args.dataset / "images"
+    reference_sfm_path = args.dataset / "sparse/0"
     pairs_path = args.outputs / "pairs-netvlad.txt"
+    # pairs_path = args.outputs / "pairs-exhaustive.txt"
     sfm_dir = args.outputs / "sfm"
     args.outputs.mkdir(parents=True, exist_ok=True)
+
+    image_list = list(map(lambda x: x.name, images_path.glob("*.*")))
+
+    image = read_image(images_path / image_list[0])
+    height, width = image.shape[:2]
+    extrinsics = read_images_binary(reference_sfm_path / "images.bin")
+    intrinsics = read_cameras_binary(reference_sfm_path / "cameras.bin")
+    opts = {}
+    for intr in intrinsics.values():
+        if intr.model=="SIMPLE_PINHOLE":
+            f = intr.params[0]
+            cx = intr.params[1]
+            cy = intr.params[2]
+            opts = dict(camera_model="SIMPLE_PINHOLE", camera_params=','.join(map(str, (f, cx, cy))))
+        elif intr.model=="SIMPLE_RADIAL":
+            f = intr.params[0]
+            cx = intr.params[1]
+            cy = intr.params[2]
+            k = intr.params[3]
+            opts = dict(camera_model="SIMPLE_RADIAL", camera_params=','.join(map(str, (f, cx, cy, k))))
+        elif intr.model=="PINHOLE":
+            fx = intr.params[0]
+            fy = intr.params[1]
+            cx = intr.params[2]
+            cy = intr.params[3]
+            opts = dict(camera_model="PINHOLE", camera_params=','.join(map(str, (fx, fy, cx, cy))))
+        else:
+            assert False, "Not supported!"
 
     retrieval_conf = extract_features.confs["netvlad"]
     matcher_conf = match_dense.confs["roma"]
 
-    retrieval_path = extract_features.main(retrieval_conf, images, args.outputs)
-    pairs_from_retrieval.main(retrieval_path, pairs_path, num_matched=3)
+    # retrieval_path = extract_features.main(retrieval_conf, images_path, args.outputs)
+    # pairs_from_retrieval.main(retrieval_path, pairs_path, num_matched=3)
 
-    features_path, matches_path = match_dense.main(matcher_conf, pairs_path, images, args.outputs, max_kps=None)
+    pairs_from_exhaustive.main(pairs_path, list(map(lambda x: x.name, images_path.glob("*.*"))))
 
-    triangulation.main(
-        sfm_dir,
-        reference_sfm,
-        images,
+    features_path, matches_path = match_dense.main(
+        matcher_conf,
         pairs_path,
-        features_path,
-        matches_path
+        images_path,
+        args.outputs,
+        max_kps=None
     )
 
-    # model = reconstruction.main(sfm_dir, images, pairs_path, features_path, matches_path)
+    # triangulation.main(
+    #     sfm_dir,
+    #     reference_sfm_path,
+    #     images_path,
+    #     pairs_path,
+    #     features_path,
+    #     matches_path
+    # )
+
+    model = reconstruction.main(
+        sfm_dir,
+        images_path,
+        pairs_path,
+        features_path,
+        matches_path,
+        image_options=opts,
+        mapper_options=dict(ba_refine_focal_length=False, ba_refine_extra_params=False),
+    )
